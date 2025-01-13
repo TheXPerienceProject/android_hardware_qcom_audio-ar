3137075960c7f6ee3555ce7f55a6468218dfa791
John Galt <johngaltfirstrun@gmail.com>
2025-01-13 11:47:02 -0500

    Revert lvacfs feature
    Squashed commit of the following:
    
    commit 7f684790af6310804947502717c99c72a0118c1d
    Author: John Galt <johngaltfirstrun@gmail.com>
    Date:   Mon Jan 13 11:46:51 2025 -0500
    
        Revert "hal: Add LVACFS microphone feature"
    
        This reverts commit 01d740fa86d928db4eb34c1c0d7a908515ec6fbb.
    
    commit 2479c447e5c360cc325a50f07d632418ce110820
    Author: John Galt <johngaltfirstrun@gmail.com>
    Date:   Mon Jan 13 11:46:46 2025 -0500
    
        Revert "add TARGET_PROVIDES_AUDIO_MANIFEST"
    
        This reverts commit 3c4b2ab8293b22f9228b95addc8323cb794cdaad.
    
    commit a109c4944e639839a58b39adf9fcfb169a8e7b1b
    Author: John Galt <johngaltfirstrun@gmail.com>
    Date:   Mon Jan 13 11:46:39 2025 -0500
    
        Revert "lvacfs: set correct format and hardcode samples for sm8650"
    
        This reverts commit e31178a365f76a7fe64bba86618c023bbb4786b1.

--- hal/AudioStream.cpp
+++ hal/AudioStream.cpp
@@ -4585,10 +4585,6 @@ int StreamInPrimary::Standby() {
     if (ret)
         ret = -EINVAL;
 
-    if (lvacfs.getWrapperOps() && lvacfs_instance) {
-        lvacfs.stopInputStream(this);
-    }
-
     stream_mutex_.unlock();
     AHAL_DBG("Exit ret: %d", ret);
     return ret;
@@ -5126,10 +5122,6 @@ int StreamInPrimary::Open() {
         }
     }
 
-    if (lvacfs.getWrapperOps() && !lvacfs_instance) {
-        lvacfs.startInputStream(this);
-    }
-
 set_buff_size:
     if (usecase_ == USECASE_AUDIO_RECORD_MMAP) {
         inBufSize = MMAP_PERIOD_SIZE * audio_bytes_per_frame(
@@ -5404,10 +5396,6 @@ ssize_t StreamInPrimary::read(const void *buffer, size_t bytes) {
         memset(palBuffer.buffer, 0, palBuffer.size);
     }
 
-    if (lvacfs.getWrapperOps() && lvacfs_instance) {
-        lvacfs.processInputStream(this, palBuffer.buffer, palBuffer.size);
-    }
-
 exit:
     if (mBytesRead <= UINT64_MAX - bytes) {
         mBytesRead += bytes;
@@ -5467,7 +5455,6 @@ StreamInPrimary::StreamInPrimary(audio_io_handle_t handle,
     audio_source_t source) :
     StreamPrimary(handle, devices, config),
     mAndroidInDevices(devices),
-    lvacfs(Lvacfs::getInstance()),
     flags_(flags),
     btSinkMetadata{0, nullptr},
     pal_vui_handle_(nullptr),
